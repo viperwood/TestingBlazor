b522dc076292469b9d03f33fd5b6e94834549d7a
viperwood <maximzabrodotsky@yandex.ru>
2024-01-12 17:20:08 +0300

    BlazorV2

---
+++ BlazorApp1/Models/UserInfo.cs
@@ -0,0 +1,9 @@
+namespace BlazorApp1.Models;
+
+public class UserInfo
+{
+    public int Id { get; set; }
+    public string? FullName { get; set; }
+    public string? Login { get; set; }
+    public string? Password { get; set; }
+}
