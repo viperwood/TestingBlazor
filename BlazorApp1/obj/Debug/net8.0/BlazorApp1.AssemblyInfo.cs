886acd043829d79d8062230bed123ca3c37cc3ce
viperwood <maximzabrodotsky@yandex.ru>
2024-01-16 15:36:31 +0300

    BlazorV5

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c44a262cb0d308b7a7b4bf4327f6e47234fa892a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab40a4b13851aecb513a110e30d1c28a259fb88c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
