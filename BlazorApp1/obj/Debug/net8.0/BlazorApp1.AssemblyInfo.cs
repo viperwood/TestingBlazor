b681849a2b6a0488f030d9e97f3845e01123917c
viperwood <maximzabrodotsky@yandex.ru>
2024-01-18 17:35:45 +0300

    BlazorV7

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+886acd043829d79d8062230bed123ca3c37cc3ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb0f3c5b7e71df001a09c2a46b63855a579bbd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
